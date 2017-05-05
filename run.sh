472be41755e21024900a40296d74fd50a5adbd10
zhy <zhyack@126.com>
2017-05-05 14:04:43 +0800

    auto_push

--- run.sh
+++ run.sh
@@ -5,60 +5,9 @@ export TRAIN_TARGETS=./data/train_output_text.txt
 export DEV_SOURCES=./data/dev_input_text.txt
 export DEV_TARGETS=./data/dev_output_text.txt
 export DEV_TARGETS_REF=./data/dev_output_text.txt
-export TRAIN_STEPS=200000
-export MODEL_DIR=./largemodel
-export EVAL_N=2000
-python -m bin.train   --config_paths="
-      ./example_configs/nmt_large.yml,
-      ./example_configs/train_seq2seq.yml,
-      ./example_configs/text_metrics_simple.yml"   --model_params "
-      vocab_source: $VOCAB_SOURCE
-      vocab_target: $VOCAB_TARGET"   --input_pipeline_train "
-    class: ParallelTextInputPipeline
-    params:
-      source_files:
-        - $TRAIN_SOURCES
-      target_files:
-        - $TRAIN_TARGETS"   --input_pipeline_dev "
-    class: ParallelTextInputPipeline
-    params:
-       source_files:
-        - $DEV_SOURCES
-       target_files:
-        - $DEV_TARGETS"   --batch_size 32   --train_steps $TRAIN_STEPS  --output_dir $MODEL_DIR --eval_every_n_steps $EVAL_N
-
-export PRED_DIR=${MODEL_DIR}/pred
-mkdir -p ${PRED_DIR}
-python -m bin.infer \
-  --tasks "
-    - class: DecodeText" \
-  --model_dir $MODEL_DIR \
-  --input_pipeline "
-    class: ParallelTextInputPipeline
-    params:
-      source_files:
-        - $DEV_SOURCES" \
-  >  ${PRED_DIR}/predictions.txt
-
-./bin/tools/multi-bleu.perl ${DEV_TARGETS_REF} < ${PRED_DIR}/predictions.txt
-# ./bin/tools/multi-bleu.perl ${TRAIN_TARGETS} < ${PRED_DIR}/predictions.txt
-
-./bin/tools/multi-bleu.perl ${PRED_DIR}/dev_output_text_ori.txt < ${PRED_DIR}/predict_full.txt
-
-
-
-
-
-export VOCAB_SOURCE=./data/dict_src
-export VOCAB_TARGET=./data/dict_dst
-export TRAIN_SOURCES=./data/train_input_text.txt
-export TRAIN_TARGETS=./data/train_output_text.txt
-export DEV_SOURCES=./data/dev_input_text.txt
-export DEV_TARGETS=./data/dev_output_text.txt
-export DEV_TARGETS_REF=./data/dev_output_text.txt
 export TRAIN_STEPS=120000
 export MODEL_DIR=./finalmodel
-export EVAL_N=5000
+export EVAL_N=1000
 python -m bin.train   --config_paths="
       ./example_configs/nmt_medium.yml,
       ./example_configs/train_seq2seq.yml,
@@ -78,7 +27,7 @@ python -m bin.train   --config_paths="
        target_files:
         - $DEV_TARGETS"   --batch_size 32   --train_steps $TRAIN_STEPS  --output_dir $MODEL_DIR --eval_every_n_steps $EVAL_N
 
-export PRED_DIR=${MODEL_DIR}/pred
+export PRED_DIR=../webnlg/data
 mkdir -p ${PRED_DIR}
 python -m bin.infer \
   --tasks "
@@ -91,6 +40,10 @@ python -m bin.infer \
         - $DEV_SOURCES" \
   >  ${PRED_DIR}/predictions.txt
 
+cd ../webnlg/
+python data_utils.py
+
+cd ../seq2seq/
 ./bin/tools/multi-bleu.perl ${DEV_TARGETS_REF} < ${PRED_DIR}/predictions.txt
 # ./bin/tools/multi-bleu.perl ${TRAIN_TARGETS} < ${PRED_DIR}/predictions.txt
 
