60446559a826295e458988dd30360cbd9e878473
zhy <zhyack@126.com>
2017-07-17 17:11:45 +0800

    best A(L)B 21 0.00015(f) pass(ave)

--- src/rl/model_utils.py
+++ src/rl/model_utils.py
@@ -33,3 +33,17 @@ def instanceOfInitModel(sess, config):
     sess.run(tf.global_variables_initializer())
     print('Model Initialized.')
     return ret
+
+
+def create_learning_rate_decay_fn(decay_steps=200, decay_rate=0.01, decay_type='natural_exp_decay', start_decay_at=0, stop_decay_at=200000, min_learning_rate=None, staircase=False):
+
+    def decay_fn(learning_rate, global_step):
+        decay_type_fn = getattr(tf.train, decay_type)
+        decayed_learning_rate = decay_type_fn(learning_rate=learning_rate, global_step=tf.minimum(global_step, stop_decay_at) - start_decay_at, decay_steps=decay_steps, decay_rate=decay_rate, staircase=staircase, name="decayed_learning_rate")
+        final_lr = tf.train.piecewise_constant(x=global_step, boundaries=[start_decay_at], values=[learning_rate, decayed_learning_rate])
+
+        if min_learning_rate:
+            final_lr = tf.maximum(final_lr, min_learning_rate)
+        return final_lr
+
+    return decay_fn
